c26f3a10c402216630978ef4623c45b8025db0a7
FJohnHRoss <rossjohn936@gmail.com>
2022-09-29 21:36:13 -0700

    reformated

--- week1/day3/LoopDLoop/LoopDLoop2.js
+++ week1/day3/LoopDLoop/LoopDLoop2.js
@@ -2,17 +2,17 @@ var candy = 0;
 var speed = 5.5
 var runnerSpeed = 5.6
 
-if(runnerSpeed > speed){
-  for(var miles = 1; miles <= 6; miles++){
+if (runnerSpeed > speed) {
+  for (var miles = 1; miles <= 6; miles++) {
     console.log(miles + " miles ran")
-    if(candy >= 0 && miles % 2 === 0){
+    if (candy >= 0 && miles % 2 === 0) {
       candy = candy + 1
       console.log(candy + " candy dispensed")
     }
   }
 }
-else{
-  for(var miles = 1; miles <= 6; miles++){
+else {
+  for (var miles = 1; miles <= 6; miles++) {
     console.log(miles + " miles ran")
   }
 }
@@ -21,5 +21,5 @@ else{
 // the starting point of the loop is 0.
 // the loop should stop at 3 because every 2 miles the runner gets a candy so I cut it in half.
 // it will know when to stop because of "candy <= 3;" and "miles <= 2;".
-// I have both of my loops increasing by + 1 because of how I have candy setup. If I would have it += 2 it would print 0, 2, 4... 
+// I have both of my loops increasing by + 1 because of how I have candy setup. If I would have it += 2 it would print 0, 2, 4...
 // we need the following variables, candy, miles
