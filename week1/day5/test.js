c5221afd8d316fe75385e382b05f74ad3ae63650
FJohnHRoss <rossjohn936@gmail.com>
2022-09-30 21:00:33 -0700

    update

--- week1/day5/test.js
+++ week1/day5/test.js
@@ -2,12 +2,12 @@ var arr = ["rock", "paper", "sissors"];
 
 
 function shuffle(arr) {
-  var currentIndex = arr.length, randomIndex;
-  while (currentIndex != 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+  var currentIndex = arr.length, randomIndex; // setting currentIndex to arr.length and randomIndex
+  while (currentIndex != 0) { // if current index is not equal to 0 the following code will run
+    randomIndex = Math.floor(Math.random() * currentIndex); // dont know exactly whats happening here
+    currentIndex--; 
+    [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]]; // dont know exactly whats happening here
   }
 }
-shuffle(arr);
-console.log(arr[0])
+shuffle(arr); // this shuffles the array
+console.log(arr[0]) // this is logging the first string within the array because its a rock paper sissors game
