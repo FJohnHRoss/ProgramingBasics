34623f0f0dd14e52919cf8439518f67fe2f97bff
FJohnHRoss <rossjohn936@gmail.com>
2022-09-30 19:17:37 -0700

    update

--- week1/day5/test.js
+++ week1/day5/test.js
@@ -1,31 +1,13 @@
-var arr = [0,1,1];
+var arr = ["rock", "paper", "sissors"];
 
 
 function shuffle(arr) {
-  
-  let currentIndex = arr.length,  randomIndex;
-
-  // While there remain elements to shuffle.
+  var currentIndex = arr.length, randomIndex;
   while (currentIndex != 0) {
-
-    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // And swap it with the current element.
-    [arr[currentIndex], arr[randomIndex]] = [
-      arr[randomIndex], arr[currentIndex]];
-  }
-  for (var i = 0; i < 10; i++) {
-    if (arr[i] == 0) {
-      console.log("table");
-    }
-    if (arr[i] == 1) {
-      console.log("chair")
-    }
-    if (arr[i] == 2) {
-      console.log("dogs")
-    }
+    [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
   }
 }
 shuffle(arr);
+console.log(arr[0])
